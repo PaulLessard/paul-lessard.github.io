c26356392c3460a1688ea27a561b1cd596960e16
Dominic Verity <dominic.verity@mq.edu.au>
2021-12-15 14:13:55 +1100

    convert to lanyon theme

--- site.hs
+++ site.hs
@@ -1,8 +1,11 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
+import           Data.Monoid                   (mappend)
+import           Data.List                     (sortBy)
+import           Data.Ord                      (comparing)
 import           Hakyll
-
+import           Control.Monad                 (liftM, forM_)
+import           System.FilePath               (takeBaseName)
 
 --------------------------------------------------------------------------------
 config :: Configuration
@@ -12,7 +15,7 @@ config = defaultConfiguration
 
 main :: IO ()
 main = hakyllWith config $ do
-    match "images/*" $ do
+    match ("images/*" .||. "js/*") $ do
         route   idRoute
         compile copyFileCompiler
 
@@ -20,54 +23,180 @@ main = hakyllWith config $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
+    match "error/*" $ do
+        route $ (gsubRoute "error/" (const "") `composeRoutes` setExtension "html")
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+            >>= applyAsTemplate siteCtx
+            >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> siteCtx)
+
+    match "pages/*" $ do
+        route $ setExtension "html"
+        compile $ do
+            pageName <- takeBaseName . toFilePath <$> getUnderlying
+            let pageCtx = constField pageName "" `mappend`
+                          baseNodeCtx
+            let evalCtx = functionField "get-meta" getMetadataKey `mappend`
+                          functionField "eval" (evalCtxKey pageCtx)
+            let activeSidebarCtx = sidebarCtx (evalCtx <> pageCtx)
+
+            pandocCompiler
+                >>= saveSnapshot "page-content"
+                >>= loadAndApplyTemplate "templates/page.html"    siteCtx
+                >>= loadAndApplyTemplate "templates/default.html" (activeSidebarCtx <> siteCtx)
+                >>= relativizeUrls
+
+    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+
+    match "posts/*" $ version "meta" $ do
+        route   $ setExtension "html"
+        compile getResourceBody
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+        compile $ do
+            posts <- loadAll ("posts/*" .&&. hasVersion "meta")
+            let taggedPostCtx = (tagsField "tags" tags) `mappend`
+                                postCtx `mappend`
+                                (relatedPostsCtx posts 3)
+
+            pandocCompiler
+                >>= saveSnapshot "content"
+                >>= loadAndApplyTemplate "templates/post.html" taggedPostCtx
+                >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> siteCtx)
+                >>= relativizeUrls
 
     create ["archive.html"] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAllSnapshots ("posts/*" .&&. hasNoVersion) "content"
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+                    constField "title" "Archive"             `mappend`
+                    constField "archive" ""                  `mappend`
+                    siteCtx
 
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> archiveCtx)
                 >>= relativizeUrls
 
+    paginate <- buildPaginateWith postsGrouper "posts/*" postsPageId
 
-    match "index.html" $ do
+    paginateRules paginate $ \page pattern -> do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAllSnapshots (pattern .&&. hasNoVersion) "content"
             let indexCtx =
+                    constField "title" (if page == 1 then "Home"
+                                                     else "Blog posts, page " ++ show page) `mappend`
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+                    constField "home" "" `mappend`
+                    paginateContext paginate page `mappend`
+                    siteCtx
 
-            getResourceBody
+            makeItem ""
                 >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= loadAndApplyTemplate "templates/index.html" indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" (baseSidebarCtx <> indexCtx)
                 >>= relativizeUrls
 
-    match "templates/*" $ compile templateCompiler
+    match "templates/*" $ compile templateBodyCompiler
 
+    create ["atom.xml"] $ do
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx `mappend`
+                    bodyField "description"
+            posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots ("posts/*" .&&. hasNoVersion) "content"
+            renderAtom feedConfig feedCtx posts
 
 --------------------------------------------------------------------------------
+
+postsGrouper :: (MonadFail m, MonadMetadata m) => [Identifier] -> m [[Identifier]]
+postsGrouper = liftM (paginateEvery 3) . sortRecentFirst
+
+postsPageId :: PageNumber -> Identifier
+postsPageId n = fromFilePath $ if (n == 1) then "index.html" else show n ++ "/index.html"
+
+--------------------------------------------------------------------------------
+
+feedConfig :: FeedConfiguration
+feedConfig = FeedConfiguration
+    { feedTitle       = "lanyon-hakyll: Lanyon Theme on Hakyll"
+    , feedDescription = "A Fork of Lanyon based on Poole"
+    , feedAuthorName  = "Heuna Kim"
+    , feedAuthorEmail = "ai@heuna-kim.net"
+    , feedRoot        = "https://github.com/hahey/lanyon-hakyll"
+    }
+
+--------------------------------------------------------------------------------
+
+siteCtx :: Context String
+siteCtx =
+    baseCtx `mappend`
+    constField "site_description" "Lanyon Theme on Hakyll" `mappend`
+    constField "site-url" "https://github.com/hahey/lanyon-hakyll" `mappend`
+    constField "tagline" "A Fork of Lanyon based on Poole" `mappend`
+    constField "site-title" "lanyon-hakyll" `mappend`
+    constField "copy-year" "2020" `mappend`
+    constField "github-repo" "https://github.com/hahey/lanyon-hakyll" `mappend`
+    defaultContext
+
+baseCtx =
+    constField "baseurl" "http://localhost:8000"
+
+--------------------------------------------------------------------------------
+
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
 
+tagsRulesVersioned tags rules =
+    forM_ (tagsMap tags) $ \(tag, identifiers) ->
+        rulesExtraDependencies [tagsDependency tags] $
+            create [tagsMakeId tags tag] $
+                rules tag identifiers
+
+relatedPostsCtx
+  :: [Item String]  -> Int  -> Context String
+relatedPostsCtx posts n = listFieldWith "related_posts" postCtx selectPosts
+  where
+    rateItem ts i = length . filter (`elem` ts) <$> (getTags $ itemIdentifier i)
+    selectPosts s = do
+      postTags <- getTags $ itemIdentifier s
+      let trimmedItems = filter (not . matchPath s) posts
+      take n . reverse <$> sortOnM (rateItem postTags) trimmedItems
+
+matchPath :: Item String -> Item String -> Bool
+matchPath x y = eqOn (toFilePath . itemIdentifier) x y
+
+eqOn :: Eq b => (a -> b) -> a -> a -> Bool
+eqOn f x y = f x == f y
+
+sortOnM :: (Monad m, Ord b) => (a -> m b) -> [a] -> m [a]
+sortOnM f xs = map fst . sortBy (comparing snd) . zip xs <$> mapM f xs
+
+--------------------------------------------------------------------------------
+
+sidebarCtx :: Context String -> Context String
+sidebarCtx nodeCtx =
+    listField "list_pages" nodeCtx (loadAllSnapshots ("pages/*" .&&. hasNoVersion) "page-content") `mappend`
+    defaultContext
+
+baseNodeCtx :: Context String
+baseNodeCtx =
+    urlField "node-url" `mappend`
+    titleField "title" `mappend`
+    baseCtx
+
+baseSidebarCtx = sidebarCtx baseNodeCtx
+
+evalCtxKey :: Context String -> [String] -> Item String -> Compiler String
+evalCtxKey context [key] item = (unContext context key [] item) >>= \cf ->
+        case cf of
+            StringField s -> return s
+            _             -> error $ "Internal error: StringField expected"
+
+getMetadataKey :: [String] -> Item String -> Compiler String
+getMetadataKey [key] item = getMetadataField' (itemIdentifier item) key
