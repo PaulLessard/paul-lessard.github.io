bdbd9c83a69e9345623ddc927f22fab01905bc43
Dominic Verity <dominic.verity@mq.edu.au>
2022-01-20 11:20:54 +1100

    Incomplete refactor to process svgtocairo output

--- src/Compilers.hs
+++ src/Compilers.hs
@@ -1,6 +1,5 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -9,6 +8,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE CPP #-}
 
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# OPTIONS_GHC -fno-warn-redundant-constraints -O2 #-}
 --------------------------------------------------------------------------------
 
@@ -24,6 +24,7 @@ module Compilers
     ) where
 
 import           Hakyll
+import           XMLWalker
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -32,7 +33,7 @@ import qualified Data.ByteString.Lazy as LB
 import qualified Data.Map as M
 import           Data.Char                     (isDigit, isSpace, isAlpha)
 import           Data.Maybe                    (fromMaybe, mapMaybe)
-import           Data.List                     (isPrefixOf, stripPrefix, sort)
+import           Data.List                     (isPrefixOf, stripPrefix, sort, intersperse)
 import           Data.Fixed                    (Fixed)
 import           Data.Functor                  ((<&>))
 import           Data.Traversable
@@ -61,7 +62,7 @@ import           Control.Monad.Except
 import           Text.Pandoc
 import           Text.Pandoc.Walk
 import           Text.Pandoc.Shared            (filteredFilesFromArchive)
-import           Text.Pandoc.Builder
+import           Text.Pandoc.Builder as B
 import           Text.Pandoc.Parsing           (runF, defaultParserState, extractIdClass)
 import           Text.Pandoc.Readers.Markdown  (yamlToMeta)
 import           Text.Blaze.Html5.Attributes   (xmlns, item)
@@ -72,12 +73,13 @@ buildLatex :: Item String -> Compiler (Item TmpFile)
 buildLatex item = do
     latexFile@(TmpFile latexPath) <- newTmpFile "lualatex.tex"
     unsafeCompiler $ writeFile latexPath $ itemBody item
-    latexToPDF latexPath >>= makeItem . TmpFile
+    runLuaLaTeX [] latexPath >>= makeItem . TmpFile
 
-latexToPDF :: FilePath  -> Compiler FilePath
-latexToPDF latexPath = do
-    exitCode <- unsafeCompiler $ system $ unwords ["lualatex", "-halt-on-error",
-        "-output-directory", takeDirectory latexPath, latexPath, ">/dev/null", "2>&1"]
+runLuaLaTeX :: [String] -> FilePath  -> Compiler FilePath
+runLuaLaTeX extraOpts latexPath = do
+    exitCode <- unsafeCompiler $ system $ unwords $
+        ["lualatex", "-halt-on-error"] ++ extraOpts ++
+        ["-output-directory", takeDirectory latexPath, latexPath, ">/dev/null", "2>&1"]
     id <- getUnderlying
     let idPath = toFilePath id
         logDir = takeDirectory idPath </> "_texlog"
@@ -141,27 +143,25 @@ domsDefaultXMLRenderSettings = X.def { X.rsXMLDeclaration = False }
 -------------------------------------------------------------------------------
 -- Load standard pandoc option sets
 
-{-# NOINLINE latexOptions #-}
-latexOptions :: Meta
-latexOptions = unsafePerformIO $ do
-    yaml <- LB.readFile "pandoc/latexOptions.yaml"
+{-# NOINLINE commonOptions #-}
+commonOptions :: Meta
+commonOptions = unsafePerformIO $ do
+    yaml <- LB.readFile "pandoc/commonOptions.yaml"
     runIOorExplode $ yamlToMeta domsDefaultReaderOptions Nothing yaml
 
-replaceMetaField :: T.Text -> MetaValue -> Meta -> Meta
-replaceMetaField nm mv meta = Meta $ M.insert nm mv (unMeta meta)
+{-# NOINLINE pdfGenOptions #-}
+pdfGenOptions :: Meta
+pdfGenOptions = unsafePerformIO $ do
+    yaml <- LB.readFile "pandoc/pdfGenOptions.yaml"
+    meta <- runIOorExplode $ yamlToMeta domsDefaultReaderOptions Nothing yaml
+    return (meta <> commonOptions)
 
-addToStartMetaList :: T.Text -> MetaValue -> Meta -> Meta
-addToStartMetaList nm mv meta =
-    case lookupMeta nm meta of
-        Just (MetaList ls)      -> replaceMetaField nm (MetaList (mv:ls)) meta
-        Just x                  -> replaceMetaField nm (MetaList [mv,x]) meta
-        Nothing                 -> replaceMetaField nm mv meta
-
+{-# NOINLINE imgGenOptions #-}
 imgGenOptions :: Meta
-imgGenOptions =
-    addToStartMetaList "header-includes"
-        (MetaInlines [RawInline "latex" "\\PassOptionsToPackage{active}{preview}"]) $
-        replaceMetaField "fontsize" (toMetaValue ("17pt" :: String)) latexOptions
+imgGenOptions = unsafePerformIO $ do
+    yaml <- LB.readFile "pandoc/imgGenOptions.yaml"
+    meta <- runIOorExplode $ yamlToMeta domsDefaultReaderOptions Nothing yaml
+    return (meta <> commonOptions)
 
 -- Meta is a monoid and when applying <> options in its rhs override corresponding 
 -- options in its lhs.
@@ -215,7 +215,7 @@ renderPandocPDF =
         (writeLaTeX domsDefaultLaTeXWriterOptions) addOpts >=> buildLatex
     where
         addOpts :: Pandoc -> Compiler Pandoc
-        addOpts (Pandoc meta body) = return $ Pandoc (meta <> latexOptions) body
+        addOpts (Pandoc meta body) = return $ Pandoc (meta <> pdfGenOptions) body
 
 pandocPDFCompiler :: Compiler (Item TmpFile)
 pandocPDFCompiler = getResourceBody >>= renderPandocPDF
@@ -260,7 +260,7 @@ data ImageInfo = ImageInfo
     , width :: Points
     } deriving (Show)
 
-getEqnDimens :: FilePath -> Compiler [(Int, Int, ImageInfo)]
+getEqnDimens :: FilePath -> Compiler [ImageInfo]
 getEqnDimens fp = unsafeCompiler $
         mapMaybe (evalStateT parseImageDimens) . lines <$> readFile fp
     where
@@ -271,12 +271,9 @@ getEqnDimens fp = unsafeCompiler $
             token "pt"
             return $ read $ n1 ++ "." ++ n2
 
-        parseImageDimens :: ParserMonad (Int, Int, ImageInfo)
+        parseImageDimens :: ParserMonad ImageInfo
         parseImageDimens = do
-            token "Preview: img#"
-            i <- number
-            stripSpaces
-            token "eqn#"
+            token "Preview: eqn#"
             e <- number
             stripSpaces
             token "dims"
@@ -286,9 +283,10 @@ getEqnDimens fp = unsafeCompiler $
             d2 <- parseDimen
             token ","
             d3 <- parseDimen
-            return (i, e, ImageInfo (d1 / ptConvFactor)
-                                    (d2 / ptConvFactor)
-                                    (d3 / ptConvFactor))
+            return $ ImageInfo
+                (d1 / ptConvFactor)
+                (d2 / ptConvFactor)
+                (d3 / ptConvFactor)
 
 -------------------------------------------------------------------------------
 -- Build an HTML file with embedded SVG sections from an input containing 
@@ -325,202 +323,135 @@ embedEquationImages imgs = flip evalStateT 0 . walkM transformEquation
                     return $ RawInline "html" (transformAndRenderImage num img typ)
         transformEquation x = return x
 
-makeEquationImages :: FilePath -> Pandoc -> Compiler (M.Map Int (X.Document, ImageInfo))
-makeEquationImages latexPath doc =
-    if numEqns > 0
+prependMeta :: Meta -> Pandoc -> Pandoc
+prependMeta pmeta (Pandoc meta body) = Pandoc (pmeta <> meta) body
+
+getMeta :: Pandoc -> Meta
+getMeta (Pandoc meta _) = meta
+
+makeEquationSVGs :: Pandoc -> Compiler [T.Text]
+makeEquationSVGs inputDoc =
+    if null eqnBlocks
     then do
-            unsafeCompiler $ writeFile latexPath latex
-            pdfPath <- latexToPDF latexPath
-            imgInfo <- getEqnDimens $ latexPath -<.> "log"
-            svgFiles <- pdfToSVGs pdfPath
-            imgData <- unsafeCompiler $ traverse (X.readFile X.def) svgFiles
-            return $ M.fromList $
-                mapMaybe (\(i, e, d) -> (e,) . (,d) <$> M.lookup i imgData) imgInfo
-    else return M.empty
+        latexFile@(TmpFile latexPath) <- newTmpFile "eqnimages.tex"
+        unsafeCompiler $ writeFile latexPath imgGenLaTeX
+        pdfPath <- runLuaLaTeX ["--shell-escape"] latexPath
+        imgInfo <- getEqnDimens $ latexPath -<.> "log"
+        svg <- unsafeCompiler $ X.readFile X.def $ pdfPath -<.> "svg"
+        return $ splitSVGAndRender imgInfo svg
+    else
+        return []
     where
-        transformEquation :: Inline -> State Int Inline
-        transformEquation (Math typ text) = do
-            modify (+1)
-            num <- get
-            return $ Math typ $ T.pack $ "\\begin{shipper}{" ++ (
-                case typ of
+        queryEquations :: Inline -> Blocks
+        queryEquations (Math typ text) =
+            rawBlock "latex" $ T.concat
+                [ "\\begin{shipper}{"
+                , case typ of
                     InlineMath -> "\\textstyle"
                     DisplayMath -> "\\displaystyle"
-                ) ++ "}{" ++ show num ++ "}" ++ T.unpack text ++ "\\end{shipper}"
-        transformEquation x = return x
+                , "}\n"
+                , text
+                , "\n\\end{shipper}\n"
+                ]
+        transformEquation x = mempty
 
-        addImgGenOptions :: Pandoc -> Pandoc
-        addImgGenOptions (Pandoc meta body) =
-            Pandoc (meta <> imgGenOptions) body
+        eqnBlocks :: Blocks
+        eqnBlocks = query queryEquations inputDoc
 
-        addShippers :: Pandoc -> (Pandoc, Int)
-        addShippers = flip runState 0 . walkM transformEquation
+        imgGenDoc :: Pandoc
+        imgGenDoc =
+            prependMeta imgGenOptions $
+                prependMeta (getMeta inputDoc) $
+                doc eqnBlocks
 
-        transDoc :: Pandoc
-        numEqns :: Int
-        (transDoc, numEqns) = addShippers $ addImgGenOptions doc
+        imgGenLaTeX :: String
+        imgGenLaTeX = writePandocLaTeX imgGenDoc
 
-        latex :: String
-        latex = writePandocLaTeX transDoc
-
-transformAndRenderImage :: Int -> (X.Document, ImageInfo) -> MathType -> T.Text
-transformAndRenderImage num (svg, ImageInfo dp _ _) typ =
-    LT.toStrict $ X.renderText domsDefaultXMLRenderSettings $
-        R.runReader (walkM transformElement svg) allIDs
+splitSVGAndRender :: [ImageInfo] -> X.Document -> [T.Text]
+splitSVGAndRender imgInfo svg = 
+    map (LT.toStrict . X.renderText domsDefaultXMLRenderSettings) (defsDoc:allEqnImages)
     where
-        transformElement :: X.Element -> R.Reader (S.Set T.Text) X.Element
-        transformElement = transformTags . transformID . addExtraAttrAtRoot
+        namedElementNodes :: X.Name -> X.Element -> [[X.Node]]
+        namedElementNodes nm e@(X.Element enm _ nodes) | nm == enm = [nodes]
+        namedElementNodes _ _ = []
 
-        allIDs :: S.Set T.Text
-        allIDs = query queryID svg
+        allEqnImages :: [X.Document]
+        allEqnImages =
+            zipWith3 mkImageSVG [1..] imgInfo $
+                query (namedElementNodes "{http://www.w3.org/2000/svg}page") svg
+
+        mkImageSVG :: Integer -> ImageInfo -> [X.Node] -> X.Document
+        mkImageSVG num (ImageInfo dp ht wd) nodes =
+            X.Document
+                (X.Prologue [] Nothing [])
+                (X.Element
+                    "{http://www.w3.org/2000/svg}svg"
+                    (M.fromList
+                        [ ("version", "1.2")
+                        , ("height", T.pack (show (dp + ht) ++ "pt"))
+                        , ("width", T.pack (show wd ++ "pt"))
+                        , ("viewBox",
+                            T.concat ["0 0 ", T.pack (show wd), " ", T.pack (show (dp + ht))])
+                        , ("style",
+                            T.concat ["transform: translateY(", T.pack (show dp), "pt);"])
+                        ]) transformedNodes) []
             where
+                transformedNodes :: [X.Node]
+                transformedNodes = walk (transformTags . transformID) nodes
+
                 queryID :: X.Element -> S.Set T.Text
                 queryID (X.Element _ attr _) = maybe S.empty S.singleton (M.lookup "id" attr)
 
-        addExtraAttrAtRoot :: X.Element -> X.Element
-        addExtraAttrAtRoot e@(X.Element nm attr nodes) =
-            if nm == "{http://www.w3.org/2000/svg}svg"
-                then X.Element nm (attr <> extraRootAttr) nodes
-                else e
+                allIDs :: S.Set T.Text
+                allIDs = query queryID nodes
 
-        extraRootAttr :: M.Map X.Name T.Text
-        extraRootAttr =
-            case typ of
-                DisplayMath -> M.fromList
-                    [ ( "class", "displayed-equation" )
-                    ]
-                InlineMath -> M.fromList
-                    [ ( "class", "inline-equation" )
-                    , ( "style"
-                      , T.pack $ "transform: translateY(" ++
-                                 show (dp + 0.5) ++ "pt);" )
-                    ]
+                transformID :: X.Element -> X.Element
+                transformID e@(X.Element nm attr nodes) =
+                    case M.lookup "id" attr of
+                        Just t ->
+                            X.Element
+                                nm
+                                (M.insert "id" (T.concat ["equation-", T.pack (show num), "-", t]) attr)
+                                nodes
+                        Nothing -> e
 
-        transformID :: X.Element -> X.Element
-        transformID e@(X.Element nm attr nodes) =
-            case M.lookup "id" attr of
-                Just t ->
-                    X.Element
-                        nm
-                        (M.insert "id" (T.concat ["equation-", T.pack (show num), "-", t]) attr)
-                        nodes
-                Nothing -> e
+                transformTags :: X.Element -> X.Element
+                transformTags (X.Element nm attr nodes) = 
+                    X.Element nm (fmap transformAttrValue attr) nodes
 
-        -- return $ foldr (M.adjust transformID) attr ["id", "{http://www.w3.org/1999/xlink}href"]
+                transformAttrValue :: T.Text -> T.Text
+                transformAttrValue s =
+                    T.concat $ intersperse "#" $ head splitup:map transformTag (tail splitup)
+                    where
+                        splitup :: [T.Text]
+                        splitup = T.splitOn "#" s
 
-        transformTags :: X.Element -> R.Reader (S.Set T.Text) X.Element
-        transformTags (X.Element nm attr nodes) = do
-            transAttr <- traverse transformAttrValue attr
-            return $ X.Element nm transAttr nodes
+                        transformTag :: T.Text -> T.Text
+                        transformTag s  =
+                            if S.member (T.takeWhile (\c -> C.isAlphaNum c || c == '-') s) allIDs
+                                then T.concat ["equation-", T.pack (show num), "-", s] 
+                                else s
 
-        transformAttrValue :: T.Text -> R.Reader (S.Set T.Text) T.Text
-        transformAttrValue s = do
-            trans <- mapM transformTag (tail splitup)
-            return $ T.concat $ head splitup:trans
+        defsDoc :: X.Document
+        defsDoc = 
+            X.Document
+                (X.Prologue [] Nothing [])
+                (X.Element
+                    "{http://www.w3.org/2000/svg}svg"
+                    (M.fromList 
+                        [ ("version", "1.2")
+                        , ("height", "0pt")
+                        , ("width",  "0pt")
+                        , ("viewBox", "0 0 0 0")
+                        , ("style", "display: none;")
+                        ]) 
+                    [X.NodeElement defsElement]) []
             where
-                splitup :: [T.Text]
-                splitup = T.splitOn "#" s
-
-                transformTag :: T.Text -> R.Reader (S.Set T.Text) T.Text
-                transformTag s  = do
-                    ids <- R.ask
-                    if S.member (T.takeWhile (\c -> C.isAlphaNum c || c == '-') s) ids
-                        then return $ T.concat ["#", "equation-", T.pack (show num), "-", s]
-                        else return $ T.concat ["#", s]
-
--- Walker for XML documents
--- Incomplete: only walks Document, Node and Element nodes
-
--- Walkable instance declarations
--- Walk Document
-instance Walkable X.Document X.Document where
-    walkM f = f
-    query f = f
-
-instance Walkable X.Element X.Document where
-    walkM = walkDocumentM
-    query = queryDocument
-
-instance Walkable [X.Node] X.Document where
-    walkM = walkDocumentM
-    query = queryDocument
-
-instance Walkable X.Node X.Document where
-    walkM = walkDocumentM
-    query = queryDocument
-
--- Walk Element
-instance Walkable X.Element X.Element where
-    walkM f e = walkElementM f e >>= f
-    query f e = f e <> queryElement f e
-
-instance Walkable [X.Node] X.Element where
-    walkM = walkElementM
-    query = queryElement
-
-instance Walkable X.Node X.Element where
-    walkM = walkElementM
-    query = queryElement
-
--- Walk node list
-instance {-# OVERLAPPING #-}
-         Walkable [X.Node] [X.Node] where
-    walkM f = traverse (walkNodeM f) >=> f
-    query f nodes = f nodes <> mconcat (map (queryNode f) nodes)
-
--- Walk node
-instance Walkable X.Node X.Node where
-    walkM f n = walkNodeM f n >>= f
-    query f n = f n <> queryNode f n
-
-instance Walkable X.Element X.Node where
-    walkM = walkNodeM
-    query = queryNode
-
--- Concrete walkers
-
--- Helper methods to walk and query the components of a Document
-walkDocumentM :: (Walkable a X.Element, Applicative m, Monad m) =>
-    (a -> m a) -> X.Document -> m X.Document
-walkDocumentM f (X.Document p e ms) = do
-    e' <- walkM f e
-    return $ X.Document p e' ms
-
-queryDocument :: (Walkable a X.Element, Monoid c) =>
-    (a -> c) -> X.Document -> c
-queryDocument f (X.Document p e ms) = query f e
-
--- Helper methods to walk and query the components of an Elemement
-walkElementM :: (Walkable a [X.Node], Applicative m, Monad m) =>
-    (a -> m a) -> X.Element -> m X.Element
-walkElementM f (X.Element nm attr nodes) = do
-    nodes' <- walkM f nodes
-    return $ X.Element nm attr nodes'
-
-queryElement :: (Walkable a [X.Node], Monoid c) =>
-    (a -> c) -> X.Element -> c
-queryElement f (X.Element _ _ nodes) = query f nodes
-
--- Helper methods to walk and query the components of an Node
-walkNodeM :: (Walkable a X.Element, Applicative m, Monad m) =>
-    (a -> m a) -> X.Node -> m X.Node
-walkNodeM f (X.NodeElement e) = do
-    e' <- walkM f e
-    return $ X.NodeElement e'
-walkNodeM _ e = return e
-
-queryNode :: (Walkable a X.Element, Monoid c) =>
-    (a -> c) -> X.Node -> c
-queryNode f (X.NodeElement e) = query f e
-queryNode _ e = mempty
-
-
-
-
-
-
-
-
-
+                defsElement :: X.Element
+                defsElement = 
+                    X.Element
+                        "{http://www.w3.org/2000/svg}defs"
+                        M.empty
+                        (concat $ query (namedElementNodes "{http://www.w3.org/2000/svg}defs") svg)
 
 
